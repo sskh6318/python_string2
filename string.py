052d96a8e7ab7a6453c75d1d79b122dc1a986d98
sskh6318 <43942567+sskh6318@users.noreply.github.com>
2019-12-09 16:56:15 +0900

    test complete

--- string.py
+++ string.py
@@ -1,19 +1,21 @@
 # python exercise #1 : data manipulation
 
 a = input()
+print(len(a))
 b = input()
-
+print(len(b))
 # a와 b의 길이를 출력
 
 
 # a와 b를 대문자로 변경
-A =
-B =
+A =a.upper()
+print(A)
+B =b.upper()
+print(B)
 
 
 # A와 B를 결합하여 C를 만듬
 C = A + B
-
 
 # C를 출력
 print(C)
